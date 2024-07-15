347f2a387416c3f3843eea1b5bd2aa6b80c95912
Raman Samusevich <raman.samusevich@honeywell.com>
2024-07-15 13:37:37 +0200

    recordning a working configuration for screeing on LUMI

--- scripts/tps_screening.sh
+++ scripts/tps_screening.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --job-name=ANM_ATLAS_REFOLDED
 #SBATCH --time=44:00:0
-#SBATCH --mem 90GB
-#SBATCH --cpus-per-task 64
+#SBATCH --mem 50GB
+#SBATCH --cpus-per-task 50
 #SBATCH --partition standard-g
 #SBATCH --account=project_465000660
-#SBATCH --gres=gpu:8
+#SBATCH --gpus 8
 
 source ~/.bashrc
 conda activate tps_ml_discovery
